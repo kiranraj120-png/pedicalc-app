24f879ae913a8b4a593cd66d42d69ac879bc951a
kiranraj120-png <kiranraj120@gmail.com>
2025-10-15 19:21:57 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = 'pedi-anes-v1';
+const urlsToCache = [
+  '/', // The root directory
+  '/index.html', // Renamed main application file
+  '/manifest.json',
+  // Add icon files here
+  '/icons/icon-192x192.png',
+];
+
+// Install event: Caches all necessary assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event: Intercepts network requests and serves from cache if available
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        // No cache hit - fetch from network
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate event: Cleans up old caches
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            // Delete old caches
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
